b790eff7258981415b9aaf52cc383d1d5978949e
richotorres <richotorrex@gmail.com>
2026-06-10 11:16:26 -0500

    Fix webhook meta

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ WEBHOOK VERIFICATION
 */
 
 app.get("/webhook", (req, res) => {
+
   const verify_token = process.env.VERIFY_TOKEN;
 
   const mode = req.query["hub.mode"];
@@ -33,16 +34,24 @@ app.get("/webhook", (req, res) => {
   const challenge = req.query["hub.challenge"];
 
   if (mode && token) {
-    if (mode === "subscribe" && token === verify_token) {
+
+    if (
+      mode === "subscribe" &&
+      token === verify_token
+    ) {
+
       console.log("Webhook verificado correctamente ✅");
 
       return res.status(200).send(challenge);
+
     } else {
+
       return res.sendStatus(403);
+
     }
   }
 
-  res.sendStatus(400);
+  return res.sendStatus(400);
 });
 
 /*
@@ -52,6 +61,7 @@ RECEIVE WHATSAPP MESSAGES
 */
 
 app.post("/webhook", (req, res) => {
+
   console.log(
     "Mensaje recibido:",
     JSON.stringify(req.body, null, 2)
@@ -68,6 +78,6 @@ START SERVER
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
